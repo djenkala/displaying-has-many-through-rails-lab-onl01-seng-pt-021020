5734b11f28a69fdc633463f0dc962f308316603e
Daniel Jenkala <djenkala@yahoo.com>
2020-07-08 21:59:20 +0000

    Automatically backed up by Learn

--- db/migrate/20200708215259_create_patients.rb
+++ db/migrate/20200708215259_create_patients.rb
@@ -1,8 +1,10 @@
 class CreatePatients < ActiveRecord::Migration[5.0]
   def change
     create_table :patients do |t|
-
-      t.timestamps
+      t.string   "name"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.integer  "age"
     end
   end
 end
